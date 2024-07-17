0b015fbb1ca6155ff6196609b726f4cae5ba01d2
DanilRellax <77833594+DanilRellax@users.noreply.github.com>
2024-07-17 15:55:30 +0300

    Add files via upload

---
+++ precache-manifest.cb50757bb54e6d421813a9916b0c989f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "3aa305f9c46f08e05eb7",
+    "url": "./static/css/main.69cdaac0.chunk.css"
+  },
+  {
+    "revision": "3aa305f9c46f08e05eb7",
+    "url": "./static/js/main.863450b9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "56d24d395645c46a73c9",
+    "url": "./static/css/2.816135aa.chunk.css"
+  },
+  {
+    "revision": "56d24d395645c46a73c9",
+    "url": "./static/js/2.904a6413.chunk.js"
+  },
+  {
+    "revision": "dcc0084922d48d58d28734b005376046",
+    "url": "./static/media/avatar.dcc00849.png"
+  },
+  {
+    "revision": "1015c157a5e7ad9eb1419e494a86a286",
+    "url": "./static/media/coin.1015c157.svg"
+  },
+  {
+    "revision": "bba6aca413b9d23ba0fee7dcd668626a",
+    "url": "./static/media/rating.bba6aca4.svg"
+  },
+  {
+    "revision": "f6ee5f377e5de69776e1a313ce53945f",
+    "url": "./static/media/market.f6ee5f37.svg"
+  },
+  {
+    "revision": "f03d2cfcf7678a291ce8679adceb4e75",
+    "url": "./index.html"
+  }
+];
